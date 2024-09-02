ee8e75a569fd861642b53e6a79d6607754280095
amyadavv <amityada010213@gmail.com>
2024-09-02 18:05:26 +0530

    cookie completed duration 1.57.10

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 const PORT = process.env.PORT || 5000;
 connectToDatabase().then(() => {
-    app.listen(PORT, () => console.log("Server Open"));
+    app.listen(PORT, () => console.log("Server Open at " + PORT));
 })
     .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { connectToDatabase } from "./db/connection.js";
 
 const PORT = process.env.PORT || 5000;
 connectToDatabase().then(() => {
-  app.listen(PORT, () => console.log("Server Open"));
+  app.listen(PORT, () => console.log("Server Open at " + PORT));
 
 })
   .catch((err) => console.log(err));
